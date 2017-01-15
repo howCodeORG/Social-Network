b9971f4ac04365c17f581189e4561300159be1f6
Francis McNamee <francis@reedoo.org>
2017-01-15 01:44:14 +0000

    Added validation
    Added validation to create-account.php

--- create-account.php
+++ create-account.php
@@ -2,16 +2,39 @@
 include('classes/DB.php');
 
 if (isset($_POST['createaccount'])) {
-
         $username = $_POST['username'];
         $password = $_POST['password'];
         $email = $_POST['email'];
 
-        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>$password, ':email'=>$email));
-        echo "Success!";
+        if (!DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
 
+                if (strlen($username) >= 3 && strlen($username) <= 32) {
+
+                        if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+
+                                if (strlen($password) >= 6 && strlen($password) <= 60) {
+
+                                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                                        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                                        echo "Success!";
+                                } else {
+                                        echo 'Invalid email!';
+                                }
+                        } else {
+                                echo 'Invalid password!';
+                        }
+                        } else {
+                                echo 'Invalid username';
+                        }
+                } else {
+                        echo 'Invalid username';
+                }
+
+        } else {
+                echo 'User already exists!';
+        }
 }
-
 ?>
 
 <h1>Register</h1>
