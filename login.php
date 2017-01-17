774cbce990044c9b9ebee545273b6483f4f63336
Francis McNamee <francis@reedoo.org>
2017-01-17 02:33:27 +0000

    Added source code
    Added source code

--- login.php
+++ login.php
@@ -9,6 +9,14 @@ if (isset($_POST['login'])) {
 
                 if (password_verify($password, DB::query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'])) {
                         echo 'Logged in!';
+                        $cstrong = True;
+                        $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+                        $user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+                        DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
+
+                        setcookie("SNID", $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, TRUE);
+
+
                 } else {
                         echo 'Incorrect Password!';
                 }
