e29755473113aaced7efd0520de48d78f00594df
Francis McNamee <francis@reedoo.org>
2017-01-23 15:31:20 +0000

    Added source code
    Added source code

--- profile.php
+++ profile.php
@@ -3,22 +3,39 @@ include('./classes/DB.php');
 include('./classes/Login.php');
 
 $username = "";
-
+$isFollowing = False;
 if (isset($_GET['username'])) {
         if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))) {
 
                 $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
+                $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
+                $followerid = Login::isLoggedIn();
 
                 if (isset($_POST['follow'])) {
 
-                        $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
-                        $followerid = Login::isLoggedIn();
+                        if ($userid != $followerid) {
 
-                        if (!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
-                                DB::query('INSERT INTO followers VALUES (\'\', :userid, :followerid)', array(':userid'=>$userid, ':followerid'=>$followerid));
-                        } else {
-                                echo 'Already following!';
+                                if (!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
+                                        DB::query('INSERT INTO followers VALUES (\'\', :userid, :followerid)', array(':userid'=>$userid, ':followerid'=>$followerid));
+                                } else {
+                                        echo 'Already following!';
+                                }
+                                $isFollowing = True;
                         }
+                }
+                if (isset($_POST['unfollow'])) {
+
+                        if ($userid != $followerid) {
+
+                                if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
+                                        DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid'=>$userid, ':followerid'=>$followerid));
+                                }
+                                $isFollowing = False;
+                        }
+                }
+                if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
+                        //echo 'Already following!';
+                        $isFollowing = True;
                 }
 
         } else {
@@ -29,5 +46,13 @@ if (isset($_GET['username'])) {
 ?>
 <h1><?php echo $username; ?>'s Profile</h1>
 <form action="profile.php?username=<?php echo $username; ?>" method="post">
-        <input type="submit" name="follow" value="Follow">
+        <?php
+        if ($userid != $followerid) {
+                if ($isFollowing) {
+                        echo '<input type="submit" name="unfollow" value="Unfollow">';
+                } else {
+                        echo '<input type="submit" name="follow" value="Follow">';
+                }
+        }
+        ?>
 </form>
