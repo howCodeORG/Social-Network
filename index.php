51c24b27f9188b03ac993cbf4c5674c729dc8607
Francis McNamee <francis@reedoo.org>
2017-02-18 02:45:17 +0000

    Added source code
    Added source code

--- index.php
+++ index.php
@@ -18,6 +18,43 @@ if (isset($_POST['comment'])) {
         Comment::createComment($_POST['commentbody'], $_GET['postid'], $userid);
 }
 
+if (isset($_POST['searchbox'])) {
+        $tosearch = explode(" ", $_POST['searchbox']);
+        if (count($tosearch) == 1) {
+                $tosearch = str_split($tosearch[0], 2);
+        }
+        $whereclause = "";
+        $paramsarray = array(':username'=>'%'.$_POST['searchbox'].'%');
+        for ($i = 0; $i < count($tosearch); $i++) {
+                $whereclause .= " OR username LIKE :u$i ";
+                $paramsarray[":u$i"] = $tosearch[$i];
+        }
+        $users = DB::query('SELECT users.username FROM users WHERE users.username LIKE :username '.$whereclause.'', $paramsarray);
+        print_r($users);
+
+        $whereclause = "";
+        $paramsarray = array(':body'=>'%'.$_POST['searchbox'].'%');
+        for ($i = 0; $i < count($tosearch); $i++) {
+                if ($i % 2) {
+                $whereclause .= " OR body LIKE :p$i ";
+                $paramsarray[":p$i"] = $tosearch[$i];
+                }
+        }
+        $posts = DB::query('SELECT posts.body FROM posts WHERE posts.body LIKE :body '.$whereclause.'', $paramsarray);
+        echo '<pre>';
+        print_r($posts);
+        echo '</pre>';
+}
+
+?>
+
+<form action="index.php" method="post">
+        <input type="text" name="searchbox" value="">
+        <input type="submit" name="search" value="Search">
+</form>
+
+<?php
+
 $followingposts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers
 WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
