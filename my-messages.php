ee2913fec3fb7c9e1279a9c9d991fdecbd2ed931
Francis McNamee <francis@reedoo.org>
2017-02-26 02:18:21 +0000

    Added source code
    Added source code

--- my-messages.php
+++ my-messages.php
@@ -8,7 +8,7 @@ if (Login::isLoggedIn()) {
 }
 
 if (isset($_GET['mid'])) {
-        $message = DB::query('SELECT * FROM messages WHERE id=:mid AND receiver=:receiver OR sender=:sender', array(':mid'=>$_GET['mid'], ':receiver'=>$userid, ':sender'=>$userid))[0];
+        $message = DB::query('SELECT * FROM messages WHERE id=:mid AND (receiver=:receiver OR sender=:sender)', array(':mid'=>$_GET['mid'], ':receiver'=>$userid, ':sender'=>$userid))[0];
         echo '<h1>View Message</h1>';
         echo htmlspecialchars($message['body']);
         echo '<hr />';
