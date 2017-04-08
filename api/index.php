6cc5689f275ce8df7c73a5387abf70c852caacd2
Francis McNamee <francis@reedoo.org>
2017-04-08 01:24:15 +0100

    Added source code
    Added source code

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("DB.php");
+require_once("Mail.php");
 
 $db = new DB("127.0.0.1", "SocialNetwork", "root", "");
 
@@ -12,6 +13,61 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         }
 
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        if ($_GET['url'] == "users") {
+
+                $postBody = file_get_contents("php://input");
+                $postBody = json_decode($postBody);
+
+                $username = $postBody->username;
+                $email = $postBody->email;
+                $password = $postBody->password;
+
+
+                if (!$db->query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+
+                        if (strlen($username) >= 3 && strlen($username) <= 32) {
+
+                                if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+
+                                        if (strlen($password) >= 6 && strlen($password) <= 60) {
+
+                                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                                        if (!$db->query('SELECT email FROM users WHERE email=:email', array(':email'=>$email))) {
+
+                                                $db->query('INSERT INTO users VALUES (\'\', :username, :password, :email, \'0\', \'\')', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                                                Mail::sendMail('Welcome to our Social Network!', 'Your account has been created!', $email);
+                                                echo '{ "Success": "User Created!" }';
+                                                http_response_code(200);
+                                        } else {
+                                                echo '{ "Error": "Email in use!" }';
+                                                http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Email!" }';
+                                        http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Password!" }';
+                                        http_response_code(409);
+                                }
+                                } else {
+                                        echo '{ "Error": "Invalid Username!" }';
+                                        http_response_code(409);
+                                }
+                        } else {
+                                echo '{ "Error": "Invalid Username!" }';
+                                http_response_code(409);
+                        }
+
+                } else {
+                        echo '{ "Error": "User exists!" }';
+                        http_response_code(409);
+                }
+
+
+        }
 
         if ($_GET['url'] == "auth") {
                 $postBody = file_get_contents("php://input");
