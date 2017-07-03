27b8d728d619d7ff3808010e6883fbe0f8c73523
Francis McNamee <francis@reedoo.org>
2017-07-03 01:19:35 +0100

    Added source code.
    Added source code.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("DB.php");
+require_once("Mail.php");
 
 $db = new DB("127.0.0.1", "SocialNetwork", "root", "");
 
@@ -7,11 +8,169 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
         if ($_GET['url'] == "auth") {
 
+        } else if ($_GET['url'] == "search") {
+
+                $tosearch = explode(" ", $_GET['query']);
+                if (count($tosearch) == 1) {
+                        $tosearch = str_split($tosearch[0], 2);
+                }
+
+                $whereclause = "";
+                $paramsarray = array(':body'=>'%'.$_GET['query'].'%');
+                for ($i = 0; $i < count($tosearch); $i++) {
+                        if ($i % 2) {
+                        $whereclause .= " OR body LIKE :p$i ";
+                        $paramsarray[":p$i"] = $tosearch[$i];
+                        }
+                }
+                $posts = $db->query('SELECT posts.id, posts.body, users.username, posts.posted_at FROM posts, users WHERE users.id = posts.user_id AND posts.body LIKE :body '.$whereclause.' LIMIT 10', $paramsarray);
+                //echo "<pre>";
+                echo json_encode($posts);
+
         } else if ($_GET['url'] == "users") {
+
+        } else if ($_GET['url'] == "comments" && isset($_GET['postid'])) {
+                $output = "";
+                $comments = $db->query('SELECT comments.comment, users.username FROM comments, users WHERE post_id = :postid AND comments.user_id = users.id', array(':postid'=>$_GET['postid']));
+                $output .= "[";
+                foreach($comments as $comment) {
+                        $output .= "{";
+                        $output .= '"Comment": "'.$comment['comment'].'",';
+                        $output .= '"CommentedBy": "'.$comment['username'].'"';
+                        $output .= "},";
+                        //echo $comment['comment']." ~ ".$comment['username']."<hr />";
+                }
+                $output = substr($output, 0, strlen($output)-1);
+                $output .= "]";
+                echo $output;
+
+        } else if ($_GET['url'] == "posts") {
+
+                $token = $_COOKIE['SNID'];
+
+                $userid = $db->query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+
+                $followingposts = $db->query('SELECT posts.id, posts.body, posts.posted_at, posts.postimg, posts.likes, users.`username` FROM users, posts, followers
+                WHERE (posts.user_id = followers.user_id
+                OR posts.user_id = :userid)
+                AND users.id = posts.user_id
+                AND follower_id = :userid
+                ORDER BY posts.likes DESC;', array(':userid'=>$userid), array(':userid'=>$userid));
+                $response = "[";
+                foreach($followingposts as $post) {
+
+                        $response .= "{";
+                                $response .= '"PostId": '.$post['id'].',';
+                                $response .= '"PostBody": "'.$post['body'].'",';
+                                $response .= '"PostedBy": "'.$post['username'].'",';
+                                $response .= '"PostDate": "'.$post['posted_at'].'",';
+                                $response .= '"PostImage": "'.$post['postimg'].'",';
+                                $response .= '"Likes": '.$post['likes'].'';
+                        $response .= "},";
+
+
+                }
+                $response = substr($response, 0, strlen($response)-1);
+                $response .= "]";
+
+                http_response_code(200);
+                echo $response;
+
+        } else if ($_GET['url'] == "profileposts") {
+                $start = (int)$_GET['start'];
+                $userid = $db->query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
+
+                $followingposts = $db->query('SELECT posts.id, posts.body, posts.posted_at, posts.postimg, posts.likes, users.`username` FROM users, posts
+                WHERE users.id = posts.user_id
+                AND users.id = :userid
+                ORDER BY posts.posted_at DESC
+                LIMIT 5
+                OFFSET '.$start.';', array(':userid'=>$userid));
+                $response = "[";
+                foreach($followingposts as $post) {
+
+                        $response .= "{";
+                                $response .= '"PostId": '.$post['id'].',';
+                                $response .= '"PostBody": "'.$post['body'].'",';
+                                $response .= '"PostedBy": "'.$post['username'].'",';
+                                $response .= '"PostDate": "'.$post['posted_at'].'",';
+                                $response .= '"PostImage": "'.$post['postimg'].'",';
+                                $response .= '"Likes": '.$post['likes'].'';
+                        $response .= "},";
+
+
+                }
+                $response = substr($response, 0, strlen($response)-1);
+                $response .= "]";
+
+                http_response_code(200);
+                echo $response;
 
         }
 
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        if ($_GET['url'] == "users") {
+
+                $postBody = file_get_contents("php://input");
+                $postBody = json_decode($postBody);
+
+                $username = $postBody->username;
+                $email = $postBody->email;
+                $password = $postBody->password;
+
+
+                if (!$db->query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+
+                        if (strlen($username) >= 3 && strlen($username) <= 32) {
+
+                                if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+
+                                        if (strlen($password) >= 6 && strlen($password) <= 60) {
+
+                                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                                        if (!$db->query('SELECT email FROM users WHERE email=:email', array(':email'=>$email))) {
+
+                                                $db->query('INSERT INTO users VALUES (\'\', :username, :password, :email, \'0\', \'\')', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                                                Mail::sendMail('Welcome to our Social Network!', 'Your account has been created!', $email);
+                                                echo '{ "Success": "User Created!" }';
+                                                http_response_code(200);
+                                        } else {
+                                                echo '{ "Error": "Email in use!" }';
+                                                http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Email!" }';
+                                        http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Password!" }';
+                                        http_response_code(409);
+                                }
+                                } else {
+                                        echo '{ "Error": "Invalid Username!" }';
+                                        http_response_code(409);
+                                }
+                        } else {
+                                echo '{ "Error": "Invalid Username!" }';
+                                http_response_code(409);
+                        }
+
+                } else {
+                        echo '{ "Error": "User exists!" }';
+                        http_response_code(409);
+                }
+
+
+        }
+
+        if ($_GET['url'] == "post") {
+                $token = $_COOKIE['SNID'];
+
+                $userid = $db->query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+                echo "Dfdf";
+        }
 
         if ($_GET['url'] == "auth") {
                 $postBody = file_get_contents("php://input");
@@ -36,6 +195,25 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
                         http_response_code(401);
                 }
 
+        } else if ($_GET['url'] == "likes") {
+                $postId = $_GET['id'];
+                $token = $_COOKIE['SNID'];
+                $likerId = $db->query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+
+                if (!$db->query('SELECT user_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$postId, ':userid'=>$likerId))) {
+
+                        $db->query('UPDATE posts SET likes=likes+1 WHERE id=:postid', array(':postid'=>$postId));
+                        $db->query('INSERT INTO post_likes VALUES (\'\', :postid, :userid)', array(':postid'=>$postId, ':userid'=>$likerId));
+                        //Notify::createNotify("", $postId);
+                } else {
+                        $db->query('UPDATE posts SET likes=likes-1 WHERE id=:postid', array(':postid'=>$postId));
+                        $db->query('DELETE FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$postId, ':userid'=>$likerId));
+                }
+
+                echo "{";
+                echo '"Likes":';
+                echo $db->query('SELECT likes FROM posts WHERE id=:postid', array(':postid'=>$postId))[0]['likes'];
+                echo "}";
         }
 
 }  else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
@@ -57,4 +235,6 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 } else {
         http_response_code(405);
 }
+
+// Helper functions
 ?>
--- profile.php
+++ profile.php
@@ -243,9 +243,111 @@ if (isset($_GET['username'])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
     <script type="text/javascript">
 
+    var start = 5;
+    var working = false;
+    $(window).scroll(function() {
+            if ($(this).scrollTop() + 1 >= $('body').height() - $(window).height()) {
+                    if (working == false) {
+                            working = true;
+                            $.ajax({
+
+                                    type: "GET",
+                                    url: "api/profileposts?username=<?php echo $username; ?>&start="+start,
+                                    processData: false,
+                                    contentType: "application/json",
+                                    data: '',
+                                    success: function(r) {
+                                            var posts = JSON.parse(r)
+                                            $.each(posts, function(index) {
+
+                                                    if (posts[index].PostImage == "") {
+
+                                                            $('.timelineposts').html(
+                                                                    $('.timelineposts').html() +
+
+                                                                    '<li class="list-group-item" id="'+posts[index].PostId+'"><blockquote><p>'+posts[index].PostBody+'</p><footer>Posted by '+posts[index].PostedBy+' on '+posts[index].PostDate+'<button class="btn btn-default" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;" data-id=\"'+posts[index].PostId+'\"> <i class="glyphicon glyphicon-heart" data-aos="flip-right"></i><span> '+posts[index].Likes+' Likes</span></button><button class="btn btn-default comment" data-postid=\"'+posts[index].PostId+'\" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;"><i class="glyphicon glyphicon-flash" style="color:#f9d616;"></i><span style="color:#f9d616;"> Comments</span></button></footer></blockquote></li>'
+                                                            )
+                                                    } else {
+                                                            $('.timelineposts').html(
+                                                                    $('.timelineposts').html() +
+
+                                                                    '<li class="list-group-item" id="'+posts[index].PostId+'"><blockquote><p>'+posts[index].PostBody+'</p><img src="" data-tempsrc="'+posts[index].PostImage+'" class="postimg" id="img'+posts[index].postId+'"><footer>Posted by '+posts[index].PostedBy+' on '+posts[index].PostDate+'<button class="btn btn-default" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;" data-id=\"'+posts[index].PostId+'\"> <i class="glyphicon glyphicon-heart" data-aos="flip-right"></i><span> '+posts[index].Likes+' Likes</span></button><button class="btn btn-default comment" data-postid=\"'+posts[index].PostId+'\" type="button" style="color:#eb3b60;background-image:url(&quot;none&quot;);background-color:transparent;"><i class="glyphicon glyphicon-flash" style="color:#f9d616;"></i><span style="color:#f9d616;"> Comments</span></button></footer></blockquote></li>'
+                                                            )
+                                                    }
+
+                                                    $('[data-postid]').click(function() {
+                                                            var buttonid = $(this).attr('data-postid');
+
+                                                            $.ajax({
+
+                                                                    type: "GET",
+                                                                    url: "api/comments?postid=" + $(this).attr('data-postid'),
+                                                                    processData: false,
+                                                                    contentType: "application/json",
+                                                                    data: '',
+                                                                    success: function(r) {
+                                                                            var res = JSON.parse(r)
+                                                                            showCommentsModal(res);
+                                                                    },
+                                                                    error: function(r) {
+                                                                            console.log(r)
+                                                                    }
+
+                                                            });
+                                                    });
+
+                                                    $('[data-id]').click(function() {
+                                                            var buttonid = $(this).attr('data-id');
+                                                            $.ajax({
+
+                                                                    type: "POST",
+                                                                    url: "api/likes?id=" + $(this).attr('data-id'),
+                                                                    processData: false,
+                                                                    contentType: "application/json",
+                                                                    data: '',
+                                                                    success: function(r) {
+                                                                            var res = JSON.parse(r)
+                                                                            $("[data-id='"+buttonid+"']").html(' <i class="glyphicon glyphicon-heart" data-aos="flip-right"></i><span> '+res.Likes+' Likes</span>')
+                                                                    },
+                                                                    error: function(r) {
+                                                                            console.log(r)
+                                                                    }
+
+                                                            });
+                                                    })
+                                            })
+
+                                            $('.postimg').each(function() {
+                                                    this.src=$(this).attr('data-tempsrc')
+                                                    this.onload = function() {
+                                                            this.style.opacity = '1';
+                                                    }
+                                            })
+
+                                            scrollToAnchor(location.hash)
+
+                                            start+=5;
+                                            setTimeout(function() {
+                                                    working = false;
+                                            }, 4000)
+
+                                    },
+                                    error: function(r) {
+                                            console.log(r)
+                                    }
+
+                            });
+                    }
+            }
+    })
+
     function scrollToAnchor(aid){
+    try {
     var aTag = $(aid);
         $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+        } catch (error) {
+                console.log(error)
+        }
     }
 
         $(document).ready(function() {
@@ -253,7 +355,7 @@ if (isset($_GET['username'])) {
                 $.ajax({
 
                         type: "GET",
-                        url: "api/profileposts?username=<?php echo $username; ?>",
+                        url: "api/profileposts?username=<?php echo $username; ?>&start=0",
                         processData: false,
                         contentType: "application/json",
                         data: '',
